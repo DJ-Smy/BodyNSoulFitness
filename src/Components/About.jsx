4552f30245022edd2e88bc2a1a517b04eea923ab
RyanGuDev <105820823+RyanGuDev@users.noreply.github.com>
2023-01-18 08:50:58 -0700

    Start Project

---
+++ src/Components/About.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import aboutimage from '../images/about.png';
+
+function About() {
+  return (
+    <div id='about'>
+        <div className='about-image'>
+            <img src={aboutimage} alt='' />
+        </div>
+        <div className='about-text'>
+            <h1>LEARN MORE ABOUT US</h1>
+            <p>alsthalkshgflkashflkshaklf</p>
+            <button>READ MORE</button>
+        </div>
+    </div>
+  )
+}
+
+export default About;
