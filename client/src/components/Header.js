27cd0a947f928e494b288b97adad6d93676f2b4d
RyanGuDev <105820823+RyanGuDev@users.noreply.github.com>
2023-01-26 10:02:58 -0700

    client

---
+++ client/src/components/Header.js
@@ -0,0 +1,119 @@
+import React, { Fragment, useContext } from 'react'
+import Avatar from '@mui/material/Avatar';
+import "./header.css"
+import { LoginContext } from './ContextProvider/Context';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import { useNavigate , NavLink } from "react-router-dom"
+
+const Header = () => {
+
+  const { logindata, setLoginData } = useContext(LoginContext);
+  //console.log(logindata);
+
+  const history = useNavigate();
+
+  const [anchorEl, setAnchorEl] = React.useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    // Logout user
+
+    const logoutuser = async()=>{
+      // 로그아웃 로직 token을 비교해서 -> token 을 사라지게 한다.
+
+      let token = sessionStorage.getItem("usersdatatoken");
+
+        const res = await fetch("/logout", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": token,
+                Accept: "application/json"
+            },
+            credentials: "include"
+        });
+
+        const data = await res.json();
+        console.log(data);
+
+        if (data.status === 201) {
+            console.log("use logout");
+            sessionStorage.removeItem("usersdatatoken");
+            setLoginData(false)
+            history("/");
+        } else {
+            console.log("error");
+        }
+    }
+
+    const goDash = () => {
+      history('/dash');
+    }
+
+    const goError = () => {
+      history("*");
+    }
+
+    const goHome = () => {
+      history('/');
+    }
+
+
+  return (
+    <>
+        <header>
+            <nav>
+                <h1 onClick={goHome}>Personal Trainer Web</h1>
+                <div className='avatar'>
+                  {
+                    logindata.ValidUserOne ? <Avatar style={{background: "salmon", fontWeight:"bold", textTransform:"capitalize"}} onClick={handleClick}>{logindata.ValidUserOne.fname[0].toUpperCase()}</Avatar>:
+                    <Avatar style={{background: "blue"}} onClick={handleClick}></Avatar>
+                  }
+                </div>
+
+                <Menu
+                        id="basic-menu"
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'basic-button',
+                        }}
+                    >
+
+                        {
+                          logindata.ValidUserOne ? (
+
+                            <>
+                              <MenuItem onClick={()=>{
+                                goDash()
+                                handleClose()}}>Profile</MenuItem>
+                              <MenuItem onClick={()=>{
+                                logoutuser()
+                                handleClose()}}>Logout</MenuItem>
+                            </>
+
+                          ) : (
+                            <>
+                              <MenuItem onClick={()=>{
+                                goError()
+                                handleClose()
+                                }}>Profile</MenuItem>
+                            </>
+                          )
+                        }
+
+      </Menu>
+            </nav>
+        </header>
+    </>
+  )
+}
+
+export default Header
---
+++ client/src/components/PasswordReset.js
@@ -0,0 +1,62 @@
+import React, {useState} from 'react'
+import { ToastContainer, toast } from 'react-toastify';
+
+const PasswordReset = () => {
+
+    const [email, setEmail] = useState("");
+
+    const [message, setMessage] = useState("");
+
+    const setVal  = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const sendLink = async(e) => {
+        e.preventDefault();
+
+        const res = await fetch("/sendpasswordlink", {
+            method: "POST",
+            headers:{
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({email})
+        });
+
+        const data = await res.json();
+
+        if(data.status === 201){
+            setEmail("");
+            setMessage(true);
+        }else{
+            toast.error("Invalid User")
+        }
+
+
+
+    }
+
+
+  return (
+    <>
+            <section>
+                <div className="form_data">
+                    <div className="form_heading">
+                        <h1>Enter Your Email</h1>
+                    </div>
+
+                    {message ? <p style={{color: "green", fontWeight: "bold"}}>password reset link send successfully in your Email</p>:""}
+                    <form>
+                        <div className="form_input">
+                            <label htmlFor="email">Email</label>
+                            <input type="email" value={email} onChange={setVal} name="email" id="email" placeholder='Enter Your Email Address' />
+                        </div>
+                        <button className='btn' onClick={sendLink}>Send</button>
+                    </form>
+                    <ToastContainer />
+                </div>
+            </section>
+        </>
+  )
+}
+
+export default PasswordReset
