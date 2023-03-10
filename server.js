5e2977da4e53bc94f79d7ff3819aec2450093968
Evan Kim <evan977512@gmail.com>
2023-03-10 15:55:21 -0700

    Heroku deploy ready

--- server.js
+++ server.js
@@ -2,17 +2,15 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-
 const dbConfig = require("./config/dbConfig");
 
 const port = process.env.PORT || 8000;
 
-
 app.use(express.json());
 
-const userRoute = require('./routes/userRoute');
-const adminRoute = require('./routes/adminRoute');
-const trainerRoute = require('./routes/trainerRoute');
+const userRoute = require("./routes/userRoute");
+const adminRoute = require("./routes/adminRoute");
+const trainerRoute = require("./routes/trainerRoute");
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
