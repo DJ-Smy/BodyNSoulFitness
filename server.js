8c41f19b93a4cf4e706493a24332dbd88d9dff01
RyanGuDev <105820823+RyanGuDev@users.noreply.github.com>
2023-03-13 09:27:48 -0600

    Heroku Deploy

--- server.js
+++ server.js
@@ -4,17 +4,25 @@ require("dotenv").config();
 
 const dbConfig = require("./config/dbConfig");
 
-const port = process.env.PORT || 8000;
-
 app.use(express.json());
 
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const trainerRoute = require("./routes/trainerRoute");
+const path = require("path");
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/trainer", trainerRoute);
 
+if (process.env.NODE_ENV === "production") {
+  app.use("/", express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => res.send("Hello World!!"));
 app.listen(port, () => console.log(`Listening on port ${port}!`));
-// wow
