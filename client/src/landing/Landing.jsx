73180ad01082a38da2dbf2c1d3e52987af657913
EvanKim <evan977512@gmail.com>
2023-04-01 11:45:04 -0600

    Join page almost doen still need to fix design

--- client/src/landing/Landing.jsx
+++ client/src/landing/Landing.jsx
@@ -5,7 +5,6 @@ import { Programs } from "./components/Programs/Programs";
 import { Reasons } from "./components/Reasons/Reasons";
 import Plans from "./components/Plans/Plans";
 import Testimonials from "./components/Testimonials/Testimonials";
-import Join from "./components/Join/Join";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer/Footer";
 
@@ -17,7 +16,6 @@ function Landing() {
       <Reasons />
       <Plans />
       <Testimonials />
-      <Join />
       <Contact />
       <Footer />
     </div>
--- client/src/landing/components/Contact/Contact.jsx
+++ client/src/landing/components/Contact/Contact.jsx
@@ -1,19 +1,19 @@
 import React, { useRef } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import emailjs from "@emailjs/browser";
-import styled from "styled-components";
+import "./contact.scss";
 
-// npm i @emailjs/browser
-
-const Contact = () => {
+function App() {
   const form = useRef();
 
   const sendEmail = (e) => {
     e.preventDefault();
 
-    emailjs.sendForm("service_y0qz4hz", "template_lx5uvbi", form.current, "BVWfFDaP9b9-yxmSp").then(
+    emailjs.sendForm("service_xgnk3cf", "template_wos7f9n", form.current, "NeESDcyDd_06p3S7D").then(
       (result) => {
         console.log(result.text);
-        console.log("message sent");
+        alert("Success send email");
       },
       (error) => {
         console.log(error.text);
@@ -23,73 +23,125 @@ const Contact = () => {
 
   return (
     <React.Fragment>
-      <div className="right-r">
-        <span>Contact Us</span>
+      <div className="Join" id="join-us">
+        <div className="left-j">
+          <hr />
+          <div>
+            <span className="stroke-text">READY TO</span>
+            <span>LEVEL UP</span>
+          </div>
+          <div>
+            <span>YOUR BODY</span>
+            <span className="stroke-text">WITH US?</span>
+          </div>
+        </div>
       </div>
-      <StyledContactForm>
-        <form ref={form} onSubmit={sendEmail}>
-          <label>Name</label>
-          <input type="text" name="user_name" />
-          <label>Email</label>
-          <input type="email" name="user_email" />
-          <label>Message</label>
-          <textarea name="message" />
-          <label>Comment</label>
-          <input type="text" name="comment" />
-          <input type="submit" value="Send" />
-        </form>
-      </StyledContactForm>
+
+      <section className="contact-section">
+        <div className="container">
+          <ToastContainer position="top-center" />
+          <div className="row justify-content-center">
+            <div className="col-md-10">
+              <div className="wrapper">
+                <div className="row no-gutters">
+                  <div className="col-md-6">
+                    <div className="contact-wrap w-100 p-lg-5 p-4">
+                      <h3 className="mb-4">Send us a message</h3>
+                      <form ref={form} onSubmit={sendEmail} id="contactForm" className="contactForm">
+                        <div className="row">
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <input type="text" className="form-control" name="name" placeholder="Name" />
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <input type="email" className="form-control" name="email" placeholder="Email" />
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <input type="text" className="form-control" name="subject" placeholder="Subject" />
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <textarea
+                                type="text"
+                                className="form-control"
+                                name="message"
+                                placeholder="Message"
+                                cols="30"
+                                rows="6"
+                              ></textarea>
+                            </div>
+                          </div>
+                          <div className="col-md-12">
+                            <div className="form-group">
+                              <input type="submit" value="Send Message" className="btn btn-primary" />
+                            </div>
+                          </div>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                  <div className="col-md-6 d-flex align-items-stretch">
+                    <div className="info-wrap w-100 p-lg-5 p-4 img">
+                      <h3>Contact us</h3>
+                      <p className="mb-4">We're open for any suggestion or just to have a chat</p>
+                      <div className="dbox w-100 d-flex align-items-start">
+                        <div className="icon d-flex align-items-center justify-content-center">
+                          <span className="fa fa-map-marker"></span>
+                        </div>
+                        <div className="text pl-3">
+                          <p>
+                            <span>Address:</span> 198 West 21th Street, Suite 721 New York NY 10016
+                          </p>
+                        </div>
+                      </div>
+                      <div className="dbox w-100 d-flex align-items-center">
+                        <div className="icon d-flex align-items-center justify-content-center">
+                          <span className="fa fa-phone"></span>
+                        </div>
+                        <div className="text pl-3">
+                          <p>
+                            <span>Phone:</span>
+                            <a href="tel://123456789">+1235 2355 98</a>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="dbox w-100 d-flex align-items-center">
+                        <div className="icon d-flex align-items-center justify-content-center">
+                          <span className="fa fa-paper-plane"></span>
+                        </div>
+                        <div className="text pl-3">
+                          <p>
+                            <span>Email:</span>
+                            <a href="mailto:info@yoursite.com">info@yoursite.com</a>
+                          </p>
+                        </div>
+                      </div>
+                      <div className="dbox w-100 d-flex align-items-center">
+                        <div className="icon d-flex align-items-center justify-content-center">
+                          <span className="fa fa-globe"></span>
+                        </div>
+                        <div className="text pl-3">
+                          <p>
+                            <span>Website:</span>
+                            <a href="#">yoursite.com</a>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </React.Fragment>
   );
-};
+}
 
-export default Contact;
-
-// Styles
-const StyledContactForm = styled.div`
-  width: 400px;
-  margin: 0 auto;
-
-  form {
-    display: flex;
-    align-items: flex-start;
-    flex-direction: column;
-    width: 100%;
-    font-size: 16px;
-    input {
-      width: 100%;
-      height: 35px;
-      padding: 7px;
-      outline: none;
-      border-radius: 5px;
-      border: 1px solid rgb(220, 220, 220);
-      &:focus {
-        border: 2px solid rgba(0, 206, 158, 1);
-      }
-    }
-    textarea {
-      max-width: 100%;
-      min-width: 100%;
-      width: 100%;
-      max-height: 100px;
-      min-height: 100px;
-      padding: 7px;
-      outline: none;
-      border-radius: 5px;
-      border: 1px solid rgb(220, 220, 220);
-      &:focus {
-        border: 2px solid rgba(0, 206, 158, 1);
-      }
-    }
-    label {
-      margin-top: 1rem;
-    }
-    input[type="submit"] {
-      margin-top: 2rem;
-      cursor: pointer;
-      background: rgb(249, 105, 14);
-      color: white;
-      border: none;
-    }
-  }
-`;
+export default App;
