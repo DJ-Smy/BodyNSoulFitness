8bcad45f568cb4a18e72ca3a5ba35ced4df4437e
RyanGuDev <105820823+RyanGuDev@users.noreply.github.com>
2023-04-18 13:26:43 -0600

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -115,9 +115,10 @@ router.post("/sendPasswordLink", async (req, res) => {
         from: "bodynsoulfitness2023@gmail.com",
         to: email,
         subject: "Sending Email for password reset",
-        text: `This Link valid for 2 minutes http://localhost:3000/forgot-password/${user.id}/${setUserToken.verifyToken}`,
+        text: `This Link valid for 2 minutes http://capstone.ryangudev.com/forgot-password/${user.id}/${setUserToken.verifyToken}`,
       };
-
+      //http://capstone.ryangudev.com/forgot-password
+      //http://localhost:3000
       transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
           return res
